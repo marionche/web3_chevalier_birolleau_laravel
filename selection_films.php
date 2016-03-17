52dcca1d3463bf0fc4c570967cf6e4a71b20b49c
Marion CHEVALIER <marionchev@hotmail.fr>
2016-03-17 10:42:52 +0100

    test

---
+++ selection_films.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>TP3 Formulaire</title>
+<head>
+<body>
+<form method="post" action="affichage_films.php">
+	<input type="text" name="recherche"/>
+	<input type="submit" value="Envoyer"/>
+</form>
+
+</body>
+</html>
